fc5da57573f0b5998610b1ac9746dc469dbbd3e7
Eugen <trushin00332211@gmail.com>
2026-03-31 20:45:22 +0500

    Тест класса Product

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,3 +17,6 @@ def category_phones():
     return Category("Смартфоны", "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
                          [product1, product2, product3])
 
+def test_product():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    assert product1.name == "Samsung Galaxy S23 Ultra"
