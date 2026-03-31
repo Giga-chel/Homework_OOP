7653ea269095508949715f767aebb05671afe0c5
Eugen <trushin00332211@gmail.com>
2026-03-31 20:44:51 +0500

    Создаем фикстуры для тестов и добавляем импорты

---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+import pytest
+from src.main import Category, Product
+
+@pytest.fixture(autouse=True)
+def reset_counters():
+    Category.category_count = 0
+    Category.product_count = 0
+    yield
+    Category.category_count = 0
+    Category.product_count = 0
+
+@pytest.fixture
+def category_phones():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    return Category("Смартфоны", "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                         [product1, product2, product3])
+
