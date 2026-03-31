f47c0ecf31cb38653d7247381690366f0d521558
Eugen <trushin00332211@gmail.com>
2026-03-31 21:41:16 +0500

    Корректировка тестов

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,6 +20,9 @@ def category_phones():
 def test_product():
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     assert product1.name == "Samsung Galaxy S23 Ultra"
+    assert product1.description == "256GB, Серый цвет, 200MP камера"
+    assert product1.price == 180000.0
+    assert product1.quantity == 5
 
 def test_category_init(category_phones):
     assert category_phones.name == "Смартфоны"
